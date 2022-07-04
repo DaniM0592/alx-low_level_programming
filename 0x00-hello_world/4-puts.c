487c27414cf986f6f00ffe8dfa8d1b0576da5430
Daneil <daneilmehabaw@gmail.com>
2022-07-04 10:10:43 -0700

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+# include <stdio.h>
+/**
+ * Description: main - prints out the string to stdout.
+ * Return:0 if success.
+ */
+int main (void)
+{
+	puts("\"programming is like building multilingual puzzel");
+	return (0);
+}
