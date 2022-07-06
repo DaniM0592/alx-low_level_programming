5d8f9cd5d4cbe337e461719de0ff2d47a6066b32
Daneil <daneilmehabaw@gmail.com>
2022-07-06 10:22:26 -0700

    task 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 /**
- * Description: main - prints the string "_putchar" from a character array.
- * Return: 0 if success.
+ * Description: main - prints ot a string from a character array.
+ * Return: 0 if success
  */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ *Description: main - prints out a string from a character array.
+ *Return: 0 if success
+ */
+
