c2ea73f7d0552922bb9866e99e1f2447af0ebab7
Daneil <daneilmehabaw@gmail.com>
2022-07-14 09:21:49 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Append src string to th dest string.
+ * Overwrite the terminating null byte at the end of dest.
+ * then add terminating null byte.
+ *
+ * Return Always 0.
+ */
+char *_strcat(char *dest, char *src);
